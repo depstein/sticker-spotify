929fe808f1314c23b8a1801e0f873c683a2cbd99
Abril <siyunji@Abrils-macbook.local>
2020-07-24 15:45:22 -0700

    add spotify recently-play endpoint

--- webserver/app.js
+++ webserver/app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 //Specify that connections from localhost:4200 (the client app) are allowed
-app.use(cors('http://localhost:4200'));
+app.use(cors('http://localhost:8200'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,18 +20,18 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
